00c178a8da430e58e17b60caaaa65de0d5e71c95
AriaKillebrewBruehl <ariadkb@gmail.com>
2022-03-19 11:46:48 -0700

    Add random list of indices

--- memCommunicationCoding/bench_marking.cc
+++ memCommunicationCoding/bench_marking.cc
@@ -1,50 +1,81 @@
 #include "bench_marking.hh"
 
-double run_measurement(int buff_size, int num_iters, int sub_iters) {
+struct timespec diff(struct timespec start, struct timespec end) {
+    struct timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+ }
+
+uint32_t* random_list(uint32_t buff_size, int32_t sub_iters) {
+    uint32_t* randoms = (uint32_t*)malloc(sizeof(uint32_t) * sub_iters);
+
+    for (int i = 0; i < sub_iters; i++) {
+        uint32_t num = (uint32_t)(rand() % buff_size);
+        randoms[i] = num;
+    }
+
+    return randoms;
+}
+double run_measurement(uint32_t buff_size, int32_t num_iters, int32_t sub_iters) {
     struct timespec start, stop;                                    // initialize clock
 
     char* array = (char*)malloc(sizeof(char) * buff_size);          // create char array of size biff_size
-
-    double* times = (double*)malloc(sizeof(double) * num_iters);    // create array to hold sizes
+    uint32_t* random_indices = random_list(buff_size, sub_iters);
 
     double total_time = 0;
+    char c;
 
     for (int i = 0; i < num_iters; i++) {
-        int num = (int)(rand() % buff_size);                        // random index in array to avoid prefetching
 
         clock_gettime(CLOCK_MONOTONIC, &start);                     // start clock
         for (int j = 0; j < sub_iters; j++) {                       // fetch random array element sub_iters times
-            char c = array[num];
+            c = array[random_indices[j]];
         }
         clock_gettime(CLOCK_MONOTONIC, &stop);
 
-        double object_count = sub_iters;                            // set object count to number of times time was measured in subloop
-        double time_taken;                                          // calculate time taken in nanoseconds
-        // time_taken = (stop.tv_sec - start.tv_sec) * 1e9;
-        // time_taken = (time_taken + (stop.tv_nsec - start.tv_nsec)) * 1e-9;
-        // time_taken = time_taken * 1e6 / object_count;
-        time_taken = stop.tv_nsec - start.tv_nsec;
-        time_taken /= object_count;
+        struct timespec dif = diff(start, stop);
 
-        times[i] = time_taken;
+        double time_taken = dif.tv_nsec / sub_iters;
+
         total_time += time_taken;
     }
 
-    double avg_time = total_time / buff_size;                       // find average time
+    double avg_time = total_time / num_iters;                       // find average time
+
+    free(array);
+    free(random_indices);
 
     return avg_time;
 
 }
 
-int main(void) {
+int main(int argc, char* argv[]) {
+
+    // if (argc != 2) {
+    //     std::cerr << "Required arguments: max range of buffer sizes (N)" << std::endl;
+    //     return -1;
+    // }
+
+    // int32_t buff_size = atoi(argv[1]);
+    // if (buff_size & (buff_size - 1)) {
+    //     std::cerr << "Max range of buffer sizes (N) must be a power of 2" << std::endl;
+    //     return -1;
+    // }
+
     std::string header = "buffer size | mean time (ns)";
     std::cout << header << std::endl;
 
-    for (int i = 2; i <= std::pow(2, 10) ; i*= 2) {                  // for various buffer sizes
+    for (int i = 1024; i <= 4194305 ; i *= 2) {                  // for various buffer sizes
         std::cout << std::string(header.length(), '-') << std::endl;
 
         int spaces = std::log10(i);
-        double time = run_measurement(i, std::pow(i, 2), 100);       // calculate latency
+        double time = run_measurement(i, i * 50, 50);       // calculate latency
 
         std::string time_string = std::to_string(time);
         int time_spaces = time_string.length();
--- memCommunicationCoding/bench_marking.hh
+++ memCommunicationCoding/bench_marking.hh
@@ -5,4 +5,8 @@
 #include <math.h>
 #include <string>
 
+
+// typedef std::chrono::time_point<std::chrono::steady_clock> time_point_t;
+// typedef long double ns_t;
+uint32_t* random_list(uint32_t buff_size, int32_t sub_iters);
 double run_measurement(int buff_size, int num_iters);
